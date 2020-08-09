2588ac1a384e7b0f2ef8f972c1a94dd22c911f72
Thomas Thisgaard Steffensen <steffensenthomas@gmail.com>
2020-08-09 14:00:55 +0200

    Added unit test for addMessage and closeConversation method in Conversation.

--- DavDoktorTests/DavDoktorConversationTests.swift
+++ DavDoktorTests/DavDoktorConversationTests.swift
@@ -9,26 +9,30 @@
 import XCTest
 @testable import DavDoktor
 
-class DavDoktorTests: XCTestCase {
+class DavDoktorConversationTests: XCTestCase {
 
+    var conversation: Conversation!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        conversation = Conversation(id: "1234", title: "testConversation")
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddMessage() throws {
+        let message: Message = Message(id: "12", content: "test content")
+        conversation.addMessage(message: message)
+        
+        XCTAssert(conversation.messages.count > 0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testCloseConversation() throws {
+        conversation.closeConversation()
+        
+        XCTAssert(conversation.status == ConversationStatus.closed.rawValue)
     }
 
 }
